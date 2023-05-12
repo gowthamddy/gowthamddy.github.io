4a0793db87d31f68eba4d4cf2dbda5514ff43d33
gowthamddy <129777817+gowthamddy@users.noreply.github.com>
2023-05-12 19:44:50 +0530

    Delete module-4 solution directory

--- module-4 solution/js/SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function (window) {  
-    var helloSpeaker = {};
-    var speakWord = "Hello world";
-    
-    helloSpeaker.speak = function (names) {
-      console.log(speakWord + " " + names);
-    }
-        window.helloSpeaker = helloSpeaker;
-    })(window);
