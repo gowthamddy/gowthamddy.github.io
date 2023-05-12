f2dbd9d309e8453f4bfdf06b78df2f6db6f40473
gowthamddy <129777817+gowthamddy@users.noreply.github.com>
2023-05-12 20:00:23 +0530

    Update script.js

--- module-4 solution/js/script.js
+++ module-4 solution/js/script.js
@@ -1,17 +1,16 @@
 (function () {
 
-var names = ["gaddam", "gowtham", "geetha", "karthik", "nikhil", "vishnu", "srihari", "tharun", "nithin", "umesh"];
-
-for (var i = 0; i < names.length; i++) {
-
-  
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'g') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
-}
-
-})();
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    for (var i = 0; i < names.length; i++) {
+    
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } 
+      else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+    })();
